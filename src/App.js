4aef714769921cc3d309e3f219a2ad3422774501
Mundane-Editorial <uday2k17@rediffmail.com>
2025-01-22 00:34:12 +0530

    updated file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import HeroSection from './components/HeroSection/Herosection';
+import AboutUs from './components/AboutUs/Aboutus';
+import Services from './components/Services/Services';
+import PharmaGifts from './components/PharmaGifts/Pharmagifts';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
---
+++ src/components/PharmaGifts/Pharmagifts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Pharmagifts() {
+  return (
+    <div>Pharmagifts</div>
+  )
+}
+
+export default Pharmagifts;
