1baa195c3a5185beea8a0a802d027c4439a9249f
Mundane-Editorial <uday2k17@rediffmail.com>
2025-01-22 00:28:49 +0530

    created file structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <AboutUs />
+      <Services />
+      <PharmaGifts />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
