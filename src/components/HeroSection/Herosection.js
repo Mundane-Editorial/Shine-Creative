157667b780346dd0f9db6acb6c97e299da06ce09
mundane-editorial <uday2k17@rediffmail.com>
2025-03-12 16:27:35 +0530

    github conflits resolved

--- src/components/HeroSection/Herosection.js
+++ src/components/HeroSection/Herosection.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import './Herosection.css';
-import image from '../../assets/landing.jpg';
-// import fox from '../../assets/fox-1.png';
-function Herosection() {
-  return (
-    <section id="hero" className="banner_wrapper">
-      <div className="container d-flex justify-content-between align-items-center">
-        <div className="left-container text-center text-lg-start">
-          <h1><span>One Stop</span><br />Pharma printing solutions</h1>
-          <p>
-            Discover our high-quality printing solutions for the pharmaceutical industry. From labels and packaging to educational materials,
-            we deliver exceptional quality and reliability.
-            <br />
-            That's not it. We're not just about printing. We're about innovation and excellence.
-          </p>
-          <a className="main-btn mt-4" href="#aboutus">Learn More</a>
-        </div>
-        <div className="right-container text-center">
-          <img src={image} className="img-fluid" alt="Food Banner" />
-        </div>
-      </div>
-    </section>
-  );
-}
-
-export default Herosection;
-=======
 import React from 'react';
 import './Herosection.css';
 import image from '../../assets/landing.jpg';
@@ -53,4 +24,3 @@ function Herosection() {
 }
 
 export default Herosection;
->>>>>>> 7187b2e (github pages deployment test)
