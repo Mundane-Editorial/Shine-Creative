6093c08b798026814299ba423fb25c67672caaba
mundane-editorial <uday2k17@rediffmail.com>
2025-03-12 16:20:30 +0530

    github pages deployment test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import HeroSection from './components/HeroSection/Herosection';
-import AboutUs from './components/AboutUs/Aboutus';
-import Services from './components/Services/Services';
-import PharmaGifts from './components/PharmaGifts/Pharmagifts';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar.js';
+import HeroSection from './components/HeroSection/Herosection.js';
+import AboutUs from './components/AboutUs/Aboutus.js';
+import Services from './components/Services/Services.js';
+import PharmaGifts from './components/PharmaGifts/Pharmagifts.js';
+import Contact from './components/Contact/Contact.js';
+import Footer from './components/Footer/Footer.js';
 
 function App() {
   return (
